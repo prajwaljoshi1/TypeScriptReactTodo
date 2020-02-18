b198e90b8e7eda7f927bfeb04924a052f499087e
prajwal joshi <prazwal@outlook.com>
2020-02-19 00:16:07 +1100

    functional component with typescript basic

--- src/index.tsx
+++ src/index.tsx
@@ -9,28 +9,38 @@ interface AppProps {
 interface AppState {
   counter: number
 }
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { counter: 0}
-  }
 
-  onIncrement = (): void => {
-    this.setState({ counter : this.state.counter +1 });
-  }
 
-  onDecrement = (): void => {
-    this.setState({ counter : this.state.counter - 1 });
-  }
-  render() {
-    return <div className="text">
-              {this.props.color}
-              <button onClick={this.onIncrement}>Increment</button>
-              <button onClick={this.onDecrement}>Decrement</button>
-              {this.state.counter}
-            </div>
-  }
+const App = (props :AppProps): JSX.Element => {
+  return (
+    <div>
+      {props.color}
+    </div>
+  )
 }
+
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
+//     this.state = { counter: 0}
+//   }
+
+//   onIncrement = (): void => {
+//     this.setState({ counter : this.state.counter +1 });
+//   }
+
+//   onDecrement = (): void => {
+//     this.setState({ counter : this.state.counter - 1 });
+//   }
+//   render() {
+//     return <div className="text">
+//               {this.props.color}
+//               <button onClick={this.onIncrement}>Increment</button>
+//               <button onClick={this.onDecrement}>Decrement</button>
+//               {this.state.counter}
+//             </div>
+//   }
+// }
 
 
 ReactDOM.render( <App color="red"/>, document.querySelector("#root"));
